8e983d12a68f726bf1499fe2173de5873fb8d965
MM-MikeM <mmendoza@mail.com>
2023-08-02 18:38:23 -0400

    update Udacity Project files -  add terrafrom mail.tf block to features block   prevent_deletion_if_contains_resources = false - 202308021837. MM

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -3,7 +3,11 @@ provider "azurerm" {
   subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+   }
 }
 terraform {
   backend "azurerm" {
